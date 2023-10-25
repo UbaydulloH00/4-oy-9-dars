0650b92eef04eb075d6d8cad90e37ff5d80c3cb4
UbaydulloH00 <turonarsloni00@gmail.com>
2023-10-25 20:52:32 +0500

    end

---
+++ index.js
@@ -0,0 +1,110 @@
+// 1 chi masala 
+
+let obj =  {
+    name : "jhone" ,
+    age : 23,
+    father : "doe",
+    age : 55
+
+};
+
+// function copy (obj,n){
+//     let obj1;
+//     if (n == 1){
+//          obj1 = Objekt.assign({},obj);
+//     }
+//     if (n == 2){
+//          obj1 = JSON.parse(JSON.stringify(obj));
+//     }
+//     if (n == 3) {
+//         obj1 ={...obj};
+        
+//     }
+
+//     console.log(obj1);
+// }
+
+// 2 chi masala 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let arr1 = [];
+// arr.forEach (function (value,index,arr){
+//      if ( index % 2 == 0 && index != 0){
+//        arr1.push(index);
+//      }
+// });
+// console.log(arr1);
+
+
+//  3 chi masala 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let arr1 = [];
+// arr.forEach (function (value,index,arr){
+//    arr1.push(value**3);
+// });
+// console.log(arr1);
+
+
+// 4 chi masala 
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let arr1 ;
+// arr1 = arr.some((value, index)=>{
+//     return value % 5 == 0
+// })
+
+// console.log(arr1);
+
+// 5-  chi masala 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// arr.forEach (function (value,index,arr){
+//    if (value % 3 == 0){
+//     sum += value
+//    }
+// });
+// console.log(sum);
+
+// 6 chi masala
+
+// let arr = [1,2,25,3,4,5,6,7,8,9,10];
+// let a = arr.findIndex((value)=>{
+//   return value % 5 == 0;
+// });
+// console.log(a);
+
+
+
+// 7 -chi masala 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let a ;
+// a = arr.includes(25);
+// console.log(a);
+
+
+// 8 ci masala
+
+// let array = [];
+// for (const key in obj) {
+//     array.push(key);
+// };
+// console.log(array);
+
+//  9- chi masala
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// for (const x of arr) {
+//     if(x % 2 == 0){
+//         sum += x
+//     }
+// };
+// console.log(sum);
+
+
+
+
+
